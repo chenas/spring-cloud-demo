09beb86e1058514020b81cbe27ec59226204912b
chenguoshu <chenguoshu@tfzq.com>
2019-01-15 13:37:27 +0800

    update

--- client/src/main/java/com/example/demo/EurkaClientApplication.java
+++ client/src/main/java/com/example/demo/EurkaClientApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class DemoApplication {
+@RefreshScope
+public class EurkaClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EurkaClientApplication.class, args);
 	}
 
    @Value("${server.port}")
@@ -24,8 +26,15 @@ public class DemoApplication {
    
    
     @RequestMapping("/")
-    public String home() {
-        return "hello world from port " + port + " path:" + path;
+    public String home(String addressCode) {
+    	System.out.println(addressCode);
+        // "hello world from port " + port + " path:" + path;
+        return "SUCCESS";
+    }
+    
+    @RequestMapping("/hello")
+    public String hello() {
+    	return "hello called. " + path;
     }
 	
 }
